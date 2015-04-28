6d166e19d958be4d5fe486542a71ba84ae92877c
Ryan Haase <ryandhaase@gmail.com>
2015-04-28 14:32:43 -0400

    Added token creating to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
+
+  before_create :generate_access_token
+
+  def generate_access_token
+    loop do
+      self.token = SecureRandom.hex
+      break token unless self.class.exists?(token: token)
+    end
+    
+  end
 end
