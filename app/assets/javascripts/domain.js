83c02135ef3b3e8d44acb5529a0eed45fd930534
Ryan Haase <ryandhaase@gmail.com>
2015-05-06 17:24:09 -0400

    Making progress on making api calls through angular and displaying in view

--- app/assets/javascripts/domain.js
+++ app/assets/javascripts/domain.js
@@ -1,5 +1,27 @@
-var app = angular.module("Domain", []);
-app.controller("domainCtrl", function($scope) {
-  $scope.firstName = "John";
-  $scope.lastName = "Doe";
-});
+var app = angular.module("Domain", ["ngResource"])
+
+.controller('domainCtrl', function($scope, $http){
+  $http.defaults.headers.common['Authorization'] = 'Token token=dacb9f7b92c77a922f9c878ac63abc4c';
+
+  $http.get('http://localhost:3001/apps').
+    success(function(data, status, headers, config) {
+      $scope.domains = data;
+    }).
+    error(function(data, status, headers, config) {
+      console.log('Err');
+    });
+
+  // $http({
+  //   method: 'GET',
+  //   url: "http://localhost:3001/apps",
+  //   headers: {
+  //     Authorization: "Token token=dacb9f7b92c77a922f9c878ac63abc4c"
+  //   }
+  // }).then(function(data) {
+  //   $scope.domains = data;
+  // },
+  // function(err) {
+  //   console.error('Err', err);
+  // })
+
+})
